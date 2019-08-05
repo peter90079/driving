fedddd2bd476439b47cda7e07529082f3f497a07
Peter Chen <peter90079@gmail.com>
2019-08-05 22:08:45 +0800

    add america

--- driving.py
+++ driving.py
@@ -5,3 +5,9 @@ if country == 'Taiwan':
 		print('you can drive')
 	else :
 		print('you can not drive')
+elif country == 'America':
+	if age >= 16:
+		print('you can drive')
+	else :
+		print('you can not dirve')
+		
